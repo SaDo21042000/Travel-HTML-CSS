9a6844a3da29d37bd61cda0dac2226a528d8d518
SaDo21042000 <dovansa21042000@gmail.com>
2022-10-03 22:17:13 +0700

    header loading 80%

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,32 @@
-console.log("text")
+//Function dropdown navbar
+
+document.addEventListener("click", handleClick, true);
+
+const temp = [];
+temp[1] = -1;
+
+function toggleNavbar(item) {
+  let dropdownMenu = item.querySelector(".dropdown-menu");
+  temp[0] = dropdownMenu;
+
+  dropdownMenu.style.display = "none";
+
+  if (dropdownMenu.style.display == "none" && temp[1] != 1) {
+    dropdownMenu.style.display = "block";
+    temp[1] = -1;
+  } else {
+    dropdownMenu.style.display = "none";
+    temp[1] = -1;
+  }
+}
+
+function handleClick() {
+  if (
+    temp[0] != undefined &&
+    temp[0].style.display == "block" &&
+    temp[1] == -1
+  ) {
+    temp[0].style.display = "none";
+    temp[1] = 1;
+  }
+}
