85aff4673022a29f7ab41b9827b4d73e13899be9
SaDo21042000 <dovansa21042000@gmail.com>
2022-10-04 22:31:15 +0700

    searchbox loading 30%

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -7,26 +7,36 @@ temp[1] = -1;
 
 function toggleNavbar(item) {
   let dropdownMenu = item.querySelector(".dropdown-menu");
-  temp[0] = dropdownMenu;
+  //temp[0] = dropdownMenu;
+  console.log(item)
+  console.log(dropdownMenu)
 
-  dropdownMenu.style.display = "none";
+  //dropdownMenu.style.display = "block";
 
-  if (dropdownMenu.style.display == "none" && temp[1] != 1) {
-    dropdownMenu.style.display = "block";
-    temp[1] = -1;
-  } else {
-    dropdownMenu.style.display = "none";
-    temp[1] = -1;
+  window.onclick = (e) => {
+    console.log(e.target);
+    console.log(e.currentTarget)
+    if (e.target == e.currentTarget) {
+      dropdownMenu.style.display = "none";
+    }        
   }
+
+  //if (dropdownMenu.style.display == "none" && temp[1] != 1) {
+  //  dropdownMenu.style.display = "block";
+  //  temp[1] = -1;
+  //} else {
+  //  dropdownMenu.style.display = "none";
+  //  temp[1] = -1;
+  //}
 }
 
 function handleClick() {
-  if (
-    temp[0] != undefined &&
-    temp[0].style.display == "block" &&
-    temp[1] == -1
-  ) {
-    temp[0].style.display = "none";
-    temp[1] = 1;
-  }
+  //if (
+  //  temp[0] != undefined &&
+  //  temp[0].style.display == "block" &&
+  //  temp[1] == -1
+  //) {
+  //  temp[0].style.display = "none";
+  //  temp[1] = 1;
+  //}
 }
